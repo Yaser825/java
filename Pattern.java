58a7b88690a13053b9b71dc4f26b23028acf2c2d
Yaser <113042341+Yaser825@users.noreply.github.com>
2022-09-16 01:20:34 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+//wrp to  pattern program
+//yaser CG BATCH 2576
+import java.util;
+class Pattern
+{ 
+public static void main(String[]args)
+{
+int j;
+for(int i=5;i>=0;i--)
+{
+for(j=1;j<=i;j++)
+{
+System.out.println("*" +" ");
+}
+System.out.println();
+}
+}
