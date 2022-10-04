46a6bdff138dbee2937b38e9993af11bbf4bfccf
Yaser <113042341+Yaser825@users.noreply.github.com>
2022-10-04 12:49:23 +0530

    Add files via upload

---
+++ Studentdetails.java
@@ -0,0 +1,25 @@
+//wrp to Studentdetails
+//yaser- CG2576
+package Anudip1;
+import java.util.*;
+class Studentdetails{
+	int sid;
+	String Sname;
+	double marks;
+
+}
+
+
+public class Student {
+
+	public static void main(String[] args) {
+		Studentdetails s1=new Studentdetails();
+		s1.sid=12;
+		s1.Sname="yaser";
+		s1.marks=100;
+		System.out.println(s1.sid+" "+s1.Sname+" "+s1.marks);
+		
+	
+	}
+
+}
