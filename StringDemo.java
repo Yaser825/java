b4e684cccd15ef9dee122073c9fa5291d62f0dab
Yaser <113042341+Yaser825@users.noreply.github.com>
2022-11-08 23:06:03 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,21 @@
+//wrp to demo String
+//Yaser  CG BATCH 2576
+
+package p1;
+public class StringDemo
+
+{
+	
+	public static void main(String[] args)
+	
+{
+	
+	String s1 = "Abdul";
+	String s2 = "Yaser";
+	System.out.println(s1==s2);
+	System.out.println(s1.equals(s2));
+	System.out.println("yaser".replace("ya","Na"));
+	System.out.println("Abdul".concat("yaser"));
+	
+}
+}
